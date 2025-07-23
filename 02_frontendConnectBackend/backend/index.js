c4dca90f4743ba340c57f9ebe5fe2e0105f0fbe8
geekyanimesh <animesh.kumar9651@gmail.com>
2025-07-23 23:47:08 +0530

    merge after deletion

--- 02_frontendConnectBackend/backend/index.js
+++ 02_frontendConnectBackend/backend/index.js
@@ -1,18 +1,32 @@
-import express from 'express'
-import { config, configDotenv } from 'dotenv';
+import express from 'express';
+import dotenv from 'dotenv';
 
-configDotenv();
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    res.send("The server is working!");
-})
+app.get("/", (req, res) => {
+  res.send("The server is working!");
+});
 
-app.get("/login",(req,res)=>{
-    res.send("Login here!");
-})
+app.get("/login", (req, res) => {
+  res.send("Login here!");
+});
 
-app.listen(port,(req,res)=>{
-    console.log(`Server listening at port: ${port}`);
-})
+const characters = [
+  { name: "Sansa Stark", email: "sophie_turner@gameofthron.es" },
+  { name: "Daenerys Targaryen", email: "emilia_clarke@gameofthron.es" },
+  { name: "Petyr Baelish", email: "aidan_gillen@gameofthron.es" },
+  { name: "Viserys Targaryen", email: "harry_lloyd@gameofthron.es" },
+  { name: "Ned Stark", email: "sean_bean@gameofthron.es" },
+  { name: "Arya Stark", email: "maisie_williams@gameofthron.es" }
+];
+
+app.get("/characters", (req, res) => {
+  res.json(characters);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at port: ${port}`);
+});
