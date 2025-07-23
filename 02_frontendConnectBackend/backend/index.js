7f84a210e6cf1b7baa72e363ed384483486a54e4
geekyanimesh <animesh.kumar9651@gmail.com>
2025-07-23 23:24:08 +0530

    connecting frontend to backend

--- 02_frontendConnectBackend/backend/index.js
+++ 02_frontendConnectBackend/backend/index.js
@@ -1,13 +1,18 @@
-import express from 'express';
-import { configDotenv } from 'dotenv';
+import express from 'express'
+import { config, configDotenv } from 'dotenv';
+
 configDotenv();
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.get("/",(req,res)=>{
-    res.send("Server is ready");
+    res.send("The server is working!");
+})
+
+app.get("/login",(req,res)=>{
+    res.send("Login here!");
 })
 
 app.listen(port,(req,res)=>{
-    console.log(`Server listening at: ${port}`);  
+    console.log(`Server listening at port: ${port}`);
 })
